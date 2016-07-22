08b219452baea4f028700e39198f50ded3cf9c47
Arlan Jaska <ajaska@berkeley.edu>
2016-07-22 15:24:59 -0700

    Add react

--- source.js
+++ source.js
@@ -1,20 +1,115 @@
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import { parseEnrolledCoursesTable, parseShoppingCartTable } from './tableParsers';
 import { getSectionsForCCN } from './actions';
 
-for (var i = 1; i < 99999; i++) {
-        window.clearInterval(i);
+
+let formData = [];
+let enrolledCourses = [];
+let shoppingCartCourses = [];
+
+
+function initialize() {
+  // Remove any existing setInterval calls
+  for (var i = 1; i < 99999; i++) {
+    window.clearInterval(i);
+  }
+
+  formData = new FormData(document.getElementById('SSR_SSENRL_CART'));
+  let enrolledTableRows = document.querySelectorAll("tr [id^='trSTDNT_ENRL_SSVW']");
+  let shoppingCartTableRows = document.querySelectorAll("tr [id^='trSSR_REGFORM_VW']");
+  enrolledCourses = parseEnrolledCoursesTable(enrolledTableRows);
+  shoppingCartCourses = parseShoppingCartTable(shoppingCartTableRows);
+
+  document.querySelector('html').innerHTML = '<!doctype html><html><head><title>Schedule Builder</title></head><body><div id="root"></div></body></html>';
 }
 
-let initialCartFormData = new FormData(document.getElementById('SSR_SSENRL_CART'));
+initialize();
 
-let enrolledTableRows = document.querySelectorAll("tr [id^='trSTDNT_ENRL_SSVW']");
-let enrolledCourses = parseEnrolledCoursesTable(enrolledTableRows);
-let shoppingCartTableRows = document.querySelectorAll("tr [id^='trSSR_REGFORM_VW']");
-let shoppingCartCourses = parseShoppingCartTable(shoppingCartTableRows);
-document.querySelector('html').innerHTML = "<pre>"+JSON.stringify(enrolledCourses, null, '\t')+"</pre>";
-document.querySelector('html').innerHTML += "<pre>"+JSON.stringify(shoppingCartCourses, null, '\t')+"</pre>";
 
-getSectionsForCCN(33647, initialCartFormData).then(function({ formData, sections }) {
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ShoppingCart courses={ shoppingCartCourses } />
+        <EnrolledCourses courses={ enrolledCourses } />
+      </div>
+    )
+  }
+}
+
+
+class ShoppingCart extends React.Component {
+  render() {
+    if (!this.props.courses) {
+      return <div>No courses</div>
+    }
+
+    let course_header = Object.keys(this.props.courses[0]).map(
+      (key, i) => <th key={i}>{ key }</th>
+    );
+    let course_rows = this.props.courses.map((row, i) => {
+      return <tr key={i}>{Object.keys(row).map((key, i) => <td key={i}>{row[key]}</td>)}</tr>
+    });
+    return (
+      <div>
+        <div>
+          This is the shopping cart.
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>{course_header}</tr>
+            </thead>
+            <tbody>{course_rows}</tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class EnrolledCourses extends React.Component {
+  render() {
+    if (!this.props.courses) {
+      return <div>No courses</div>
+    }
+
+    let course_header = Object.keys(this.props.courses[0]).map(
+      (key, i) => <th key={i}>{ key }</th>
+    );
+    let course_rows = this.props.courses.map((row, i) => {
+      return <tr key={i}>{Object.keys(row).map((key, i) => <td key={i}>{row[key]}</td>)}</tr>
+    });
+    return (
+      <div>
+        <div>
+          This is the enrolled courses.
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>{course_header}</tr>
+            </thead>
+            <tbody>{course_rows}</tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+getSectionsForCCN(33647, formData).then(function({ formData, sections }) {
   document.querySelector('html').innerHTML += "<pre>"+JSON.stringify(sections, null, '\t')+"</pre>";
 })
+
+let root = document.getElementById('root');
+
+ReactDOM.render(
+  <App />,
+  root
+);
