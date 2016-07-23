01876914145bf1f35cd8bf1ac68f12d0c40d2594
Arlan Jaska <ajaska@uber.com>
2016-07-23 14:36:11 -0700

    Fix parsing -- was losing some lines

--- courselistparsing/courses-parser.py
+++ courselistparsing/courses-parser.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import sys
 import json
+import re
 
 sys.setrecursionlimit(15000)
 
@@ -8,6 +9,8 @@ sys.setrecursionlimit(15000)
 Course = namedtuple('Course', ['ccn', 'dept', 'dept_number', 'section', 'type', 'title']) 
 
 buildings = """
+Soda 3
+Soda 4
 Barrows
 Latimer
 Valley
@@ -61,64 +64,64 @@ Hesse 122
 """.split("\n")[1:-1]
 
 courses = set()
-with open('courses-raw.txt') as f:
-    i = 0
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        if not line.startswith('(MTWRFSU)'):
+with open('courses-oneline.txt') as f:
+    line = f.read()
+    line = line.strip()
+    line_courses = line.split('2016 Fall')
+    for line_course in line_courses:
+        line_course = line_course.strip()
+        if not re.match('[0-9]{4,} ', line_course):
             continue
-        line = line.strip()
-        line_courses = line.split('2016 Fall')
-        for line_course in line_courses[1:]:
-            line_course = line_course.strip()
-            line_course = line_course.split(' ', 5)
-            remainder = line_course[5]
+        line_course = line_course.split(' ', 5)
+        remainder = line_course[5]
 
-            remainder = remainder.split('0.00')[0]
-            remainder = remainder.split('0.50')[0]
-            remainder = remainder.split('1.00')[0]
-            remainder = remainder.split('2.00')[0]
-            remainder = remainder.split('3.00')[0]
-            remainder = remainder.split('4.00')[0]
-            remainder = remainder.split('5.00')[0]
-            remainder = remainder.split('6.00')[0]
-            remainder = remainder.split('6.50')[0]
-            remainder = remainder.split('10.50')[0]
-            remainder = remainder.split('00:00')[0]
-            remainder = remainder.split('M 12:00')[0]
-            remainder = remainder.split('TR 12:00')[0]
-            remainder = remainder.split('T 15:00')[0]
-            for building in buildings:
-                remainder = remainder.split(building)[0]
+        remainder = remainder.split('0.00')[0]
+        remainder = remainder.split('0.50')[0]
+        remainder = remainder.split('1.00')[0]
+        remainder = remainder.split('2.00')[0]
+        remainder = remainder.split('3.00')[0]
+        remainder = remainder.split('4.00')[0]
+        remainder = remainder.split('5.00')[0]
+        remainder = remainder.split('6.00')[0]
+        remainder = remainder.split('6.50')[0]
+        remainder = remainder.split('10.50')[0]
+        remainder = remainder.split('00:00')[0]
+        remainder = remainder.split('M 12:00')[0]
+        remainder = remainder.split('TR 12:00')[0]
+        remainder = remainder.split('T 15:00')[0]
+        for building in buildings:
+            remainder = remainder.split(building)[0]
 
-            remainder = remainder.strip()
+        remainder = remainder.strip()
 
-            courses.add(Course(
-                line_course[0],
-                line_course[1],
-                line_course[2],
-                line_course[3],
-                line_course[4],
-                remainder
-            ))
-        i += 1
+        new_course = Course(
+            line_course[0],
+            line_course[1],
+            line_course[2],
+            line_course[3],
+            line_course[4],
+            remainder
+        )
+        courses.add(new_course)
+        #print(new_course)
+
 courses = list(courses)
 
-# This is horrifically inefficient
+lectures = set()
+for course in courses:
+    if course.type == 'LEC':
+        lectures.add((course.dept ,course.dept_number))
+
 def filter_discussions(course):
-    if course.type != 'DIS':
+    if course.type not in ('LAB', 'DIS'):
         return True
-    possible_lecs = len(list(filter(lambda x: x.dept == course.dept and x.dept_number == course.dept_number and x.type == 'LEC', courses))) > 0
-    if possible_lecs:
+    if (course.dept, course.dept_number) in lectures:
         return False
     return True
     
 courses = sorted(courses, key=lambda x: x.ccn)
-print(courses)
+
 courses = list(filter(filter_discussions, courses))
-print(courses)
 depts = sorted(list(set(course.dept for course in courses)))
 with open('depts.json', 'w') as f:
     json.dump(depts, f, separators=(',',':'))
