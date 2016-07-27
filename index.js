0f7ea7a9bfa76491bdd425dc578ee3dec5e4edfd
Arlan Jaska <ajaska@berkeley.edu>
2016-07-26 18:18:22 -0700

    Bug fix: shopping cart wasn't loading

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function initialize() {
   if (enrolledTableRows.length > 0) {
     enrolledCourses = parseEnrolledCoursesTable(enrolledTableRows);
   }
-  if (shoppingCartCourses.length > 0) {
+  if (shoppingCartTableRows.length > 0) {
     shoppingCartCourses = parseShoppingCartTable(shoppingCartTableRows);
   }
 
