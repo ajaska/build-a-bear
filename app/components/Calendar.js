07ba2fb1fb2e549e963950c97496fb75f156e00c
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-07-29 15:20:24 -0700

    Renaming calendar css classes

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -1,48 +1,46 @@
 import React from 'react';
 
-class Calendar extends React.Component {
-  render() {
-    return (
-      <div className="cal-enroll">
-        <div className="cal-header-row">
-          <div className="cal-header-row-entry monday-column">Monday</div>
-          <div className="cal-header-row-entry">Tuesday</div>
-          <div className="cal-header-row-entry">Wednesday</div>
-          <div className="cal-header-row-entry">Thursday</div>
-          <div className="cal-header-row-entry">Friday</div>
-        </div>
-        <div className="cal-body">
-          <div className="cal-dotw-column monday-column"></div>
-          <div className="cal-dotw-column"></div>
-          <div className="cal-dotw-column"></div>
-          <div className="cal-dotw-column"></div>
-          <div className="cal-dotw-column friday-column"></div>
+function Calendar({ courses }) {
+  console.log(courses);
+  const sc = courses;
+  console.log(sc[0].lecture);
+  const lecture = Object.keys(sc).filter(course => sc[course].lecture);
+  const other = Object.keys(sc).filter(course => !sc[course].lecture);
+  const genEntries = (key) => key.map((key, i) => (
+    <div className={sc[key].day + " lecture-entry monday start-" + sc[key].start} key={i}>
+      <div className="lecture-entry-header semibold">{sc[key].start}</div>
+      <div className={"lecture-entry-body block-" + (parseInt(sc[key].length) / 30)}>{sc[key].desc} <br />{sc[key].room} </div>
+      <div className="lecture-entry-footer"></div>
+    </div>
+  ));
 
-          <div className="lecture-entry monday am-630">
-            <div className="lecture-entry-header semibold">9 - 11AM</div>
-            <div className="lecture-entry-body block-4">Math 113 Lec 006 <br /> 420 Cory Hall</div>
-            <div className="lecture-entry-footer"></div>
-          </div>
+  const lectures = genEntries(lecture);
 
-          <div className="lecture-entry thursday am-11">
-            <div className="lecture-entry-header semibold">9 - 11AM</div>
-            <div className="lecture-entry-body block-2">Math 113 Lec 006 <br /> 420 Cory Hall</div>
-            <div className="lecture-entry-footer"></div>
-          </div>
-
-          <div className=" lecture-entry wednesday am-11">
-            <div className="lecture-entry-header semibold">9 - 11AM</div>
-            <div className="lecture-entry-body block-3">Math 113 Lec 006 <br /> 420 Cory Hall</div>
-            <div className="lecture-entry-footer"></div>
-          </div>
-
-          <div className="other-entry monday am-830">
-            <div className="other-entry-body block-4"><span className="semibold">11 - 1PM</span> <br />Math 113 Lec 006 <br /> 420 Cory Hall</div>
-          </div>
-        </div>
+  return (
+    <div className="cal-enroll">
+      <div className="cal-header-row">
+        <div className="cal-header-row-entry monday-column">Monday</div>
+        <div className="cal-header-row-entry">Tuesday</div>
+        <div className="cal-header-row-entry">Wednesday</div>
+        <div className="cal-header-row-entry">Thursday</div>
+        <div className="cal-header-row-entry">Friday</div>
       </div>
-    )
-  }
+      <div className="cal-body">
+        <div className="cal-dotw-column monday-column"></div>
+        <div className="cal-dotw-column"></div>
+        <div className="cal-dotw-column"></div>
+        <div className="cal-dotw-column"></div>
+        <div className="cal-dotw-column friday-column"></div>
+
+        { lectures }
+
+        <div className="other-entry monday am-830">
+          <div className="other-entry-body block-4"><span className="semibold">11 - 1PM</span> <br />Math 113 Lec 006 <br /> 420 Cory Hall</div>
+        </div>
+
+      </div>
+    </div>
+  )
 }
 
 export default Calendar;
