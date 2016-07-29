4110725034e4014005254ffc48c4ff56fef74bc5
Arlan Jaska <ajaska@berkeley.edu>
2016-07-29 15:35:33 -0700

    Add time formatter to Calendar

--- app/containers/Calendar.js
+++ app/containers/Calendar.js
@@ -1,6 +1,46 @@
 import { connect } from 'react-redux';
 import Calendar from '../components/Calendar';
 
+function timeToInt(time, am) {
+  let timeInt = parseInt(time.replace(':', ''), 10);
+  if (!am && Math.floor(timeInt / 100) !== 12) {
+    timeInt = timeInt + 1200;
+  }
+  if (timeInt % 10 === 9) {
+    timeInt = timeInt + 1;
+  }
+  if (timeInt % 100 === 60) {
+    timeInt = timeInt + 100 - 60;
+  }
+  return timeInt;
+}
+
+function prettyTime(start, startAM, end, endAM) {
+  const startHour = (Math.floor(start / 100) - 1) % 12 + 1;
+  const startMin = start % 100;
+  const endHour = (Math.floor(end / 100) -1 ) % 12 + 1;
+  const endMin = end % 100;
+
+  let startString;
+  if (startMin === 0) {
+    startString = `${startHour}`;
+  } else {
+    startString = `${startHour}:${startMin}`;
+  }
+
+  let endString;
+  if (endMin === 0) {
+    endString = `${endHour}`;
+  } else {
+    endString = `${endHour}:${endMin}`;
+  }
+
+  if (startAM === endAM) {
+    return `${startString}-${endString} ${endAM}`;
+  }
+  return `${startString} ${startAM} - ${endString} ${endAM}`;
+}
+
 function parseTime(timeString) {
   const re = /((?:Mo|Tu|We|Th|Fr|Sa|Su)+) (\d?\d:\d\d)(AM|PM) - (\d?\d:\d\d)(AM|PM)/;
 
@@ -11,18 +51,13 @@ function parseTime(timeString) {
   }
   const [days, startTime, startAM, endTime, endAM] = matches.slice(1);
 
-  let start = parseInt(startTime.replace(':', ''), 10);
-  if (Math.floor(start / 100) !== 12 && startAM === 'PM') {
-    start = start + 1200;
-  }
-  let end = parseInt(endTime.replace(':', ''), 10);
-  if (Math.floor(end / 100) !== 12 && endAM === 'PM') {
-    end = end + 1200;
-  }
+  const start = timeToInt(startTime, startAM === 'AM');
+  let end = timeToInt(endTime, endAM === 'AM');
   if (end % 10 === 9) {
     end = end + 1;
   }
   const length = end - start;
+  const formatted = prettyTime(start, startAM, end, endAM);
 
   const daysArray = days.match(/\w\w/g);
   if (!daysArray) {
@@ -31,6 +66,7 @@ function parseTime(timeString) {
   }
   return daysArray.map(day => ({
     day,
+    formatted,
     start,
     length,
   }));
@@ -49,6 +85,7 @@ const mapStateToProps = (state) => {
       day: time.day,
       start: time.start,
       length: time.length,
+      formatted: time.formatted,
       lecture,
       waitlisted,
     })));
