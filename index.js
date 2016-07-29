23ecb18c574d0ea164f2969824d176e60f98f405
Arlan Jaska <ajaska@berkeley.edu>
2016-07-28 18:11:48 -0700

    Use new helper in initial load

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import App from './app/components/App';
 
 import { head, body } from './html';
 
-import { parseEnrolledCoursesTable, parseShoppingCartTable } from './app/lib/tableParsers';
+import { parseResponse } from './app/lib/responseParser';
 import { setShoppingCart } from './app/actions/shoppingCart';
 import { setEnrolledCourses } from './app/actions/enrolled';
 import { setFormData } from './app/actions/api';
@@ -27,16 +27,7 @@ function initialize() {
 
   document.querySelector('head').innerHTML = head;
 
-  formData = new FormData(document.getElementById('SSR_SSENRL_CART'));
-  const enrolledTableRows = document.querySelectorAll("tr [id^='trSTDNT_ENRL_SSVW']");
-  const shoppingCartTableRows = document.querySelectorAll("tr [id^='trSSR_REGFORM_VW']");
-  if (enrolledTableRows.length > 0) {
-    enrolledCourses = parseEnrolledCoursesTable(enrolledTableRows);
-  }
-  if (shoppingCartTableRows.length > 0 &&
-      !shoppingCartTableRows[0].innerText.includes('shopping cart is empty')) {
-    shoppingCartCourses = parseShoppingCartTable(shoppingCartTableRows);
-  }
+  ({ formData, enrolledCourses, shoppingCartCourses } = parseResponse(document));
 }
 
 function goReact() {
