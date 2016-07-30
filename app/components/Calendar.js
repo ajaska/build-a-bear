01f353954cbcd9cf5a6268d5689ff9789adc7411
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-07-29 21:46:42 -0700

    Bugfix: Calendar height was improper value

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -6,22 +6,30 @@ function Calendar({ courses }) {
   const lecture = Object.keys(sc).filter(course => sc[course].lecture);
   const other = Object.keys(sc).filter(course => !sc[course].lecture);
 
-  var earliestClass = Math.min.apply(Math, sc.map( function(a) {return a.start;} ))
-  var latestClass = Math.max.apply(Math, sc.map( function(b) {return b.start;} ))
+  let earliestClassTime = Math.min.apply(Math, sc.map( function(a) {return a.start;} ));
+  let latestClassTime = Math.max.apply(Math, sc.map( function(b) {return b.start;} ));
+  let latestClass = sc.find( function (c) { return c.start == latestClassTime; });
+
+  console.log(earliestClassTime);
+  console.log(latestClassTime);
+  console.log(latestClass);
+
+  let calendarHeight = ((latestClassTime - earliestClassTime + latestClass.length) / 50) * 60 + 30;
+
+  var heightFix = {
+    height: '' + calendarHeight + 'px',
+  };
 
   const genLectures = (key) => key.map((key, i) => (
-    <div className={sc[key].day + " lecture-entry rel-start-" + (parseInt(sc[key].start) - earliestClass + 650)} key={i}>
+    <div className={sc[key].day + " lecture-entry rel-start-" + (parseInt(sc[key].start) - earliestClassTime + 650)} key={i}>
       <div className="lecture-entry-header semibold">{sc[key].formatted}</div>
       <div className={"lecture-entry-body block-" + sc[key].length}>{sc[key].desc} <br />{sc[key].room} </div>
       <div className="lecture-entry-footer"></div>
     </div>
   ));
 
-  console.log(earliestClass);
-  console.log(latestClass);
-
   const genNonLectures = (key) => key.map((key, i) => (
-    <div className={sc[key].day + " other-entry rel-start-" + (parseInt(sc[key].start) - earliestClass + 650)} key={i}>
+    <div className={sc[key].day + " other-entry rel-start-" + (parseInt(sc[key].start) - earliestClassTime + 650)} key={i}>
       <div className={"other-entry-body block-" + sc[key].length}><span className="semibold">{sc[key].formatted}</span><br />{sc[key].desc} <br />{sc[key].room} </div>
     </div>
   ));
@@ -38,7 +46,7 @@ function Calendar({ courses }) {
         <div className="cal-header-row-entry">Thursday</div>
         <div className="cal-header-row-entry">Friday</div>
       </div>
-      <div className="cal-body">
+      <div style={heightFix} className="cal-body">
         <div className="cal-dotw-column monday-column"></div>
         <div className="cal-dotw-column"></div>
         <div className="cal-dotw-column"></div>
@@ -47,7 +55,6 @@ function Calendar({ courses }) {
 
         { lectures }
         { others }
-
 
       </div>
     </div>
