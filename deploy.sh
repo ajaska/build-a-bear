4530a813356f641a8b0d53e62cef86de99fa8bf1
Arlan Jaska <ajaska@berkeley.edu>
2016-08-02 16:52:07 -0700

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ npm run-script build-css
 cp ./meme-app.js ./dist/app.js
 cp ./css/main.css ./dist/main.css
 
+git commit -m "Update dist to $(git rev-parse HEAD)" ./dist/
 git subtree push --prefix dist origin gh-pages
