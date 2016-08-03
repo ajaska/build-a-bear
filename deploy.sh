d6edce25bd6312e6a151b13401c50a13384e1c54
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-08-02 17:27:13 -0700

    Fixed issue with deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ npm run-script build
 npm run-script build-css
 
 cp ./meme-app.js ./dist/app.js
-cp ./css/main.css ./dist/main.css
+cp ./css/main.css ./dist/css/main.css
 
 git commit -m "Update dist to $(git rev-parse --short HEAD)" -- ./dist/
 git subtree push --prefix dist origin gh-pages
