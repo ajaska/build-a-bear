f59d12bb3d409a2617b8a1277455751e81d04850
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-08-04 15:31:00 -0700

    Accidentally removed ROOT_URL var

--- html.js
+++ html.js
@@ -1,6 +1,6 @@
 export const head = `
 <link href='https://fonts.googleapis.com/css?family=Arimo:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="/css/main.css">
+<link rel="stylesheet" type="text/css" href="${ROOT_URL}/css/main.css">
 <link rel="stylesheet" href="https://npmcdn.com/react-select/dist/react-select.css">
 <title>Build-a-Bear</title>
 `
