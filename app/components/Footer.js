e6ab18249298a7a0e57257a95a4fcc2970b8adbe
Arlan Jaska <ajaska@berkeley.edu>
2016-08-24 15:51:16 -0700

    Disable the Footer :^)

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import $ from 'jquery'; // eslint-disable-line import/no-unresolved
 
-import CoursePicker from '../containers/CoursePicker';
-
-class Footer extends React.Component {
-  handleFooterClick() {
-    $('.add-course-mobile').modal('show');
-  }
-
-  render() {
-    return (
-      <div className="footer">
-        <button
-          className="add-class-mobile-button"
-          onClick={this.handleFooterClick}
-        >
-          Add Classes
-        </button>
-        <div className="ui modal add-course-mobile">
-          <CoursePicker />
-        </div>
-      </div>
-    );
-  }
+function Footer() {
+  return (
+    <div className="footer">
+      Adding classes on mobile is not yet supported. Please visit bear.plus on a desktop browser.
+    </div>
+  )
 }
-// <div><span>Enrolled units: <span className="color-blue">15.0</span> (12 + 3 w/l)<br/>Units remaining: <span className="color-red">1.0</span></span></div>
 
 export default Footer;
