9fd9bb64dd6a36767e0f27c5c910a2067866ae5d
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-10-17 13:59:01 -0700

    [bugfix] Fix dropdown bolding

--- app/components/html/MyAcademics.js
+++ app/components/html/MyAcademics.js
@@ -4,9 +4,7 @@ function MyAcademics() {
   return (
     <div className="my-academics-link-wrapper">
       <img className="icon" src={`${ROOT_URL}/assets/backArrow.svg`} />
-      <a className="link" href="https://calcentral.berkeley.edu/academics">
-        Return to My Academics
-      </a>
+      <a className="link" href="https://calcentral.berkeley.edu/academics"> Return to My Academics</a>
     </div>
   );
 }
