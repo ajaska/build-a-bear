8b60c006854c9f360ea277c61c972d288f4d435c
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-10-27 12:04:08 -0700

    Users can now click on the extension to go to course adding

---
+++ extension/e.js
@@ -0,0 +1,4 @@
+console.log("Running");
+chrome.browserAction.onClicked.addListener(function(tab) {
+  window.open('https://bcsweb.is.berkeley.edu/psc/bcsprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES_2.SSR_SSENRL_CART.GBL?ucFrom=CalCentral&ucFromLink=https%3A%2F%2Fcalcentral.berkeley.edu%2Facademics%3FucUpdateCache%3Denrollment&ucFromText=Class%20Enrollment','_blank');
+});
